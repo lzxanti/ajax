4ccac13919dc19f5576ac03084bab30290d66626
lzxanti <1972401425@qq.com>
2019-09-16 14:11:14 +0800

    添加User的get/set方法和toString

--- src/com/bjsxt/pojo/User.java
+++ src/com/bjsxt/pojo/User.java
@@ -9,6 +9,35 @@ package com.bjsxt.pojo;
  * <p>Description：</p>
  */
 public class User {
-	private String name;
-	private int age;
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
