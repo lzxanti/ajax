a027cc001b913d4a77be4cd60884a5cf85fe7335
lzxanti <1972401425@qq.com>
2019-09-16 14:28:29 +0800

    删除toString方法

--- src/com/bjsxt/pojo/User.java
+++ src/com/bjsxt/pojo/User.java
@@ -12,10 +12,7 @@ public class User {
     private String name;
     private int age;
 
-    @Override
-    public String toString() {
-        return "{name:" + name + ",age:" + age + "}";
-    }
+
 
     public String getName() {
         return name;
