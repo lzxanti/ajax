16a8fe73967ab2b68552e1f7307cc83373e2f9a5
lzxanti <1972401425@qq.com>
2019-09-16 14:22:08 +0800

    修改User的构造方法,添加带参构造

--- src/com/bjsxt/pojo/User.java
+++ src/com/bjsxt/pojo/User.java
@@ -32,6 +32,11 @@ public class User {
         return age;
     }
 
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
